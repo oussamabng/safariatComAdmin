20dd15968f2eaff5c33bb4a90d9a6bfa8fd31d88
ChaTbiM <chatbimdev@gmail.com>
2020-02-04 19:05:42 +0100

    folders structure and tailwindCss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="mt-12 text-black border border-2 border-black">
+      <h1 className="text-2xl font-semibold text-gray-700 text-center">
+        You're ready to go with using <span className="text-black"> React</span>{" "}
+        and <span className="text-purple-400"> Tailwind!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
