f3273a76029cedbb7503fe004da553f4c1470deb
TarekArar <gt_arar@esi.dz>
2020-02-04 20:57:29 +0100

    navigtion fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import Header from './components/Header/Header';
+import { BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+// Scenes Components Importation
+import Home from './scenes/Home/Home.jsx';
+import Services from './scenes/Services/Services.jsx';
+import Blog from './scenes/Blog/Blog.jsx';
+import Contact from './scenes/Contact/Contact.jsx';
+import About from './scenes/About/About.jsx';
 
 function App() {
   return (
-    <div className="mt-12 text-black border border-2 border-black">
-      <h1 className="text-2xl font-semibold text-gray-700 text-center">
-        You're ready to go with using <span className="text-black"> React</span>{" "}
-        and <span className="text-purple-400"> Tailwind!</span>
-      </h1>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route component={Home} exact path="/" ></Route>
+        <Route component={Services} path="/services" ></Route>
+        <Route component={Blog} path="/blog" ></Route>
+        <Route component={Contact} path="/contact" ></Route>
+        <Route component={About} path="/about" ></Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
---
+++ src/scenes/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            Hello from Services
+        </div>
+    );
+}
+
+export default Services;
