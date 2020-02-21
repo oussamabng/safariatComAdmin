00a048aa2147257d4535981f0f04a6fd55c737c1
oussamabng <o.bengoudifa@esi-sba.dz>
2020-02-21 22:53:54 +0100

    aside added

---
+++ src/components/aside/asideAdmin.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {} from "react-bootstrap";
+import "./asideAdmin.css";
+import AsideSvg from "./images/AsideSvg.png";
+import Report from "./images/report.svg";
+import Business from "./images/business-and-finance.svg";
+import Crm from "./images/crm.svg";
+import Plus from "./images/plus.svg";
+import Compass from "./images/compass.svg";
+import Logout from "./images/logout.svg";
+class AsideAdmin extends Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    render(){
+        return(
+            <>
+            <aside className="mainAsideAdmin">
+                <div className="mainAsideSection">
+                    <div className="ImageAside">
+                        <img src={AsideSvg} alt="svg"/>
+                    </div>
+                <div className="sidebarAdmin">
+                    <ul>
+                        <li className="active">
+                            
+                            <a href="#" >
+                                <img src={Report} alt="Report"/>
+                            </a>
+                        </li>
+                        <li className="">
+                            <a href="#">
+                                <img src={Business} alt="Report"/>
+                            </a>
+                        </li >
+                        <li className="">
+                            <a href="#">
+                                <img src={Crm} alt="Report"/>
+                            </a>
+                        </li>
+                        <li className="">
+                            <a href="#">
+                                <img src={Plus} alt="Report"/>
+                            </a>
+                        </li>
+                        <li className="">
+                            <a href="#">
+                                <img src={Compass} alt="Report"/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="GetOutAdmin">
+                    <a href="#">
+                    <hr className="borderTop"/>
+                    <img src={Logout} alt="logout"/>
+                    </a>
+                </div>
+                </div>
+            </aside>
+            </>
+        )
+    }
+
+}
+
+export default AsideAdmin;
--- src/scenes/AdminDashbord/Admin.jsx
+++ src/scenes/AdminDashbord/Admin.jsx
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import HeaderAdmin from "../../components/Header/HeaderAdmin";
-
+import AsideAdmin from "../../components/aside/asideAdmin.jsx";
 class Admin extends Component{
     constructor(props){
         super(props);
@@ -10,6 +10,7 @@ class Admin extends Component{
         return(
         <>
         <HeaderAdmin/>
+        <AsideAdmin/>
         </>
         );
     }
