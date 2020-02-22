63ed3ecdf74bd1947c7b82364a16f6c0f9e0bb11
oussamabng <o.bengoudifa@esi-sba.dz>
2020-02-22 23:54:01 +0100

    circles progress graph added

--- src/components/GeneralAnalyDashbord/GeneralDashbord.jsx
+++ src/components/GeneralAnalyDashbord/GeneralDashbord.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { Progress } from 'antd';
 import "./GeneralDashbord.css";
 class GeneralDashbord extends Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={
+            percent:0,
+            percentEM:0,
+            percentS:70,
+            percentE :18
+        }
     }
-
+    componentDidMount = ()=>{
+        const interv = setInterval(()=>{
+            if (this.state.percent === this.state.percentS){
+                clearInterval(interv);
+            }else{
+                this.setState({
+                    percent : this.state.percent+1
+                });
+            }
+        },10)
+        const intervE = setInterval(()=>{
+            if (this.state.percentEM === this.state.percentE){
+                clearInterval(intervE);
+            }else{
+                this.setState({
+                    percentEM : this.state.percentEM+1
+                });
+            }
+        },10)
+    }
+    
     render(){
+   
         return(
             <>
             <section className="mainGeneralDashbord">
@@ -52,10 +79,36 @@ class GeneralDashbord extends Component{
                 </div>
             </div>
                 </div>
-                </div>
-                
+                </div> 
             </section>
+            <section className="progressCirclesAdmin">
+            <div className="circlesProgress">
+                <div className="progressWithTitle">
+                <Progress type="circle" strokeColor="#55E63A" percent={this.state.percent} width={120} strokeWidth="9"/>
+                <p>weekly</p>
+                <div className="titleCircleProg">
+                <h1>Satisfied Travellers</h1>
+                </div>
+                </div>
+                <div className="progressWithTitle">
+                <Progress type="circle" strokeColor="#E67F3A" percent={this.state.percentEM} width={120} strokeWidth="9"/>
+                <p>weekly</p>
+                <div className="titleCircleProg">
+                <h1>Satisfied Employeers</h1>
+                </div>
+                </div> 
+                <div className="progressWithTitle">
+                <Progress type="circle" strokeColor="#F8E91F" percent={this.state.percent} width={120} strokeWidth="9"/>
+                <p>weekly</p>
+                <div className="titleCircleProg">
+                <h1>Search Traffic</h1>
+                </div>
+                </div> 
+            </div>
+            </section>
+          
             </>
+            
         )
     }
 }
