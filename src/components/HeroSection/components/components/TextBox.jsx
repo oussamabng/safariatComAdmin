7cc67dc7145ea57fd4fbfd5b66266259472695db
mustapha chatbi <chatbimdev@gmail.com>
2020-03-11 02:10:37 -0800

    Resolved Another Conflict

--- src/components/HeroSection/components/components/TextBox.jsx
+++ src/components/HeroSection/components/components/TextBox.jsx
@@ -10,26 +10,9 @@ const TextBox = props => {
       <img val={2} src="women.png" alt="women"></img>
     ) : null;
   };
-<<<<<<< HEAD
-  return( 
-    <Container  {...props} className="z-10 relative"  >
-      
-        <div className="z-10 text text-xs  md:text-base">{props.text}</div>
-
-        <Person 
-        index={ props.index}
-      >
-        {showPicture()}
-      </Person> 
-        
-
-       </Container>)
-}
-=======
   return (
     <Container {...props} className="z-10 w-12/12 relative">
       <div className="z-10 text-xs sm:text-s md:text-base">{props.text}</div>
->>>>>>> 54889d2f5fd443ece58c758ecba7457df998a2cc
 
       <Person index={props.index}>{showPicture()}</Person>
     </Container>
@@ -42,29 +25,8 @@ const Container = styled.div`
   margin-top: 1rem;
   font-size: 1.1rem;
   color: #838383;
-<<<<<<< HEAD
-  width: ${props => props.index === 0 ? 27.875 : props.index === 1 ? 22.25 : 22.25 }rem;
-
-  
-  @media (max-width:768px){
-
-    width: ${props => props.index === 0 ? 16.25 : props.index === 1 ? 11 : 11 }rem;
-  }
-
-  
-
-  .text{
-    @media (max-width:768px){
-      font-size:12px;
-      word-spacing: 1px;
-
-      flex-basis:  ${props => props.index === 0 ? 15 : 11 }rem;
-    }
-  }
-=======
   width: ${props =>
     props.index === 0 ? 27.875 : props.index === 1 ? 22.25 : 22.25}rem;
->>>>>>> 54889d2f5fd443ece58c758ecba7457df998a2cc
 `;
 
 export default TextBox;
